862b78cd02824fe2d8ff00aa59b4cd1a97abd4d3
snippets-al <47079217+snippets-al@users.noreply.github.com>
2019-01-27 09:56:54 -0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-var navigateOverlay = document.querySelector('.navigate-overlay');
-var navigateMenu = document.querySelector('.navigate-menu');
-var navClassList = navigateMenu.classList;
+let navigateOverlay = document.querySelector('.navigate-overlay');
+let navigateMenu = document.querySelector('.navigate-menu');
+let navClassList = navigateMenu.classList;
+let buttonIcon = document.querySelector('nav .fa');
 function activateMenu()
 {
 	if(navClassList.contains('navigate-active') != true){
 	 	navClassList.add('navigate-active');
-	 	navigateOverlay.style.display = 'block';
-	 	
+		navigateOverlay.style.display = 'block';
+		buttonIcon.classList.remove('fa-bars');
+		buttonIcon.classList.add('fa-times');
 	}else{
 	 	navClassList.remove('navigate-active');
-	 	navigateOverlay.style.display = 'none';
+		navigateOverlay.style.display = 'none';
+		buttonIcon.classList.remove('fa-times');
+		buttonIcon.classList.add('fa-bars');
 	}	 
 }
 window.addEventListener('click', function (event){
 	if(event.target == navigateOverlay){
-	navClassList.remove('navigate-active');
-	navigateOverlay.style.display = 'none';
+		navClassList.remove('navigate-active');
+		navigateOverlay.style.display = 'none';
+		buttonIcon.classList.remove('fa-times');
+		buttonIcon.classList.add('fa-bars');
 	}
-
-})	
+});	
